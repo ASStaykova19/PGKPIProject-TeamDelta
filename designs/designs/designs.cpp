0ef8aab3338cd19ee64dbfbe9151e5e2fd81ba25
Aklingenberg19 <63726525+Aklingenberg19@users.noreply.github.com>
2020-11-19 12:13:36 +0100

    Decorations
    Header Decorations

---
+++ designs/designs/designs.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	cout << "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█" << endl;
+	cout << "█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█" << endl;
+	cout << "█░░║║║╠─║─║─║║║║║╠─░░█" << endl;
+	cout << "█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█" << endl;
+	cout << "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" << endl;
+
+
+	cout << "888888888888  88  88             888888888888              88             888888888888                          " << endl;
+	cout << "     88           88                  88                   88                  88                               " << endl;
+	cout << "     88           88                  88                   88                  88                               " << endl;
+	cout << "     88       88  88   ,d8            88       ,adPPYYba,  88   ,d8            88        ,adPPYba,    ,adPPYba, " << endl;
+	cout << "     88       88  88 ,a8              88               Y8  88, a8              88       a8       8a  a8P_____88 " << endl;
+	cout << "     88       88  8888[               88       ,adPPPPP88  8888[               88       8b       d8  8PP""""""  " << endl;
+	cout << "     88       88  88`   Yba,          88       88, ,   88  88` Yba,            88        8a,   ,a8   8b,   ,aa  " << endl;
+	cout << "     88       88  88    `Y8a          88       ` 8bbdP Y8  88   `Y8a           88        ` YbbdP '    ` Ybbd8 ' " << endl;
+
+
+
+	                                                                                                                 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+                                                                                                                 
+                                                                                                                 
+
+
+                                                                     
+}
