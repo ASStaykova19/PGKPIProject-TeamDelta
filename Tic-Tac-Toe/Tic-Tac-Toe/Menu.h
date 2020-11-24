77745db738f4318ab894af03b45c638d868e0389
Miroslav Tenev Ivanov <63789166+MTIvanov19@users.noreply.github.com>
2020-11-24 17:51:03 +0200

    Edit Menu.h to the right place

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Menu.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iomanip>
+#include "Game.h"
+#include "Info.h"
+#include "Rules.h"
+using namespace std;
+
+void menu()
+{
+    //print out game menu
+
+    int num;
+    cout << setw(4) << endl;
+    cout << setw(4) << endl;
+    cout << setw(4) << endl;
+    cout << setw(4) << endl;
+
+    cout << setw(50) << " ~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << setw(50) << "        MENU        " << endl;
+    cout << setw(50) << " ~~~~~~~~~~~~~~~~~~~~" << endl;
+
+    cout << setw(51) << "{                    }" << endl;
+    cout << setw(51) << "{      GAME  (1)     }" << endl;
+    cout << setw(51) << "{      RULES (2)     }" << endl;
+    cout << setw(51) << "{      INFO  (3)     }" << endl;
+    cout << setw(51) << "{      EXIT  (4)     }" << endl;
+    cout << setw(51) << "{                    }" << endl;
+    cout << setw(4) << endl;
+
+    cout << setw(53) << "    Enter your choice:    " << endl;   //enter operation choice
+
+    cin >> num;
+
+    cout << setw(4) << endl;
+    cout << setw(4) << endl;
+    cout << setw(4) << endl;
+
+    if (num == 1) {
+        game();     //send user to the game
+    }
+    if (num == 2) {
+        rules();   //send user to the rules
+    }
+    if (num == 3) {
+        info();     //send user to info
+    }
+    if (num == 4) {
+        exit(0);    //end the programme
+    }
+}
